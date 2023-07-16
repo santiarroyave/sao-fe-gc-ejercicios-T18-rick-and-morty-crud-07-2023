07cad9e104622642643975a5022881b6d6b227ee
santiarroyave <santi98cv@hotmail.com>
2023-07-17 00:01:12 +0200

    Agregando el model en el componente crear

--- src/app/home/lista-per/crear/crear.component.ts
+++ src/app/home/lista-per/crear/crear.component.ts
@@ -1,21 +1,33 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { MisPersonajes } from 'src/app/models/mis-personajes.model';
 
 @Component({
   selector: 'app-crear',
   templateUrl: './crear.component.html',
   styleUrls: ['./crear.component.css']
 })
-export class CrearComponent {
+export class CrearComponent implements OnInit{
 
   @Input() abierto:any;
 
-  id:number = 0;
-  name:string = "";
-  status:string ="";
-  species:string ="";
-  gender:string ="";
-  origin:string ="";
-  image:string ="";
+  // id:number = 0;
+  // name:string = "";
+  // status:string ="";
+  // species:string ="";
+  // gender:string ="";
+  // origin:string ="";
+  // image:string ="";
+
+  personaje: MisPersonajes = {
+    id: 0,
+    name: "",
+    status:"",
+    species:"",
+    gender:"",
+    origin:"",
+    image:"",
+  };
+  enviado = false;
 
   cerrar(){
     this.abierto = false;
@@ -23,5 +35,10 @@ export class CrearComponent {
 
   crear(){
     alert("Creando personaje");
+
+  }
+
+  ngOnInit(): void {
+    
   }
 }
