0f29125587f49d8bf6f963f1e56761784c13b95b
santiarroyave <santi98cv@hotmail.com>
2023-07-16 18:48:00 +0200

    Creación de proyecto

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'sao-fe-gc-ejercicios-T18-rick-and-morty-crud-07-2023';
+}
